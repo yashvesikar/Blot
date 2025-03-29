2dcea2b133cec203074cda8e08041e68119443a3
David Merfield <dmerfield@gmail.com>
2025-03-29 18:54:30 +0100

    Fixes Dropbox client

--- app/clients/dropbox/util/createClient.js
+++ app/clients/dropbox/util/createClient.js
@@ -2,7 +2,7 @@
 // and doing Dropbox.Dropbox is too upsetting
 // in every single file. I wrap the constructor.
 
-const Dropbox = require("dropbox").Dropbox;
+const { Dropbox } = require("dropbox");
 const database = require("../database");
 const config = require("config");
 
@@ -11,7 +11,7 @@ module.exports = function (blogID, callback) {
     if (err) return callback(err);
     if (!account) return callback(new Error("No Dropbox account"));
 
-    const client = new Dropbox({ fetch });
+    const client = new Dropbox();
 
     client.auth.setAccessToken(account.access_token);
 
