f04ce3bd8f280fc124595af6e215d062a6f5e3f4
David Merfield <dmerfield@gmail.com>
2025-07-21 09:36:47 +0200

    Catch remote errors

--- app/clients/icloud/remove.js
+++ app/clients/icloud/remove.js
@@ -3,13 +3,15 @@ const fs = require("fs-extra");
 const remoteDelete = require("./sync/util/remoteDelete");
 
 module.exports = async (blogID, path, callback) => {
+  const pathOnBlot = localPath(blogID, path);
 
-    const pathOnBlot = localPath(blogID, path);
-
+  try {
     await fs.remove(pathOnBlot);
     await remoteDelete(blogID, path);
+  } catch (error) {
+    console.error(`Error removing ${pathOnBlot}:`, error);
+    return callback(error);
+  }
 
-    callback();
+  callback();
 };
-
-
--- app/clients/icloud/write.js
+++ app/clients/icloud/write.js
@@ -3,13 +3,15 @@ const fs = require("fs-extra");
 const remoteUpload = require("./sync/util/remoteUpload");
 
 module.exports = async (blogID, path, contents, callback) => {
+  const pathOnBlot = localPath(blogID, path);
 
-    const pathOnBlot = localPath(blogID, path);
-
+  try {
     await fs.outputFile(pathOnBlot, contents);
     await remoteUpload(blogID, path);
+  } catch (error) {
+    console.error(`Error writing to ${pathOnBlot}:`, error);
+    return callback(error);
+  }
 
-    callback();
+  callback();
 };
-
-
