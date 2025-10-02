1d50bcee3808f791f0e1e17eaf26903edd5a7a7e
David Merfield <dmerfield@gmail.com>
2025-10-02 18:19:13 +0200

    Makes entry.list more tolerant

--- app/blog/render/load/list.js
+++ app/blog/render/load/list.js
@@ -1,20 +1,23 @@
 module.exports = function (list) {
-  if (list.length) {
-    list[list.length - 1].last = true;
+  if (!Array.isArray(list)) return [];
+
+  const len = list.length;
+
+  if (len > 0 && list[0] && typeof list[0] === 'object') {
     list[0].first = true;
   }
-
-  if (list.length > 2) {
-    list[list.length - 2].penultimate = true;
+  if (len > 0 && list[len - 1] && typeof list[len - 1] === 'object') {
+    list[len - 1].last = true;
+  }
+  if (len > 2 && list[len - 2] && typeof list[len - 2] === 'object') {
+    list[len - 2].penultimate = true;
   }
 
-  // Should I do a zero-based index or not?
-  // Decide before documenting this. Beware
-  // if you use this in your theme before then.
-  list = list.map(function (el, i) {
-    el.position = i + 1;
-    return el;
-  });
+  for (let i = 0; i < len; i++) {
+    if (list[i] && typeof list[i] === 'object') {
+      list[i].position = i + 1; // 1-based index
+    }
+  }
 
   return list;
 };
