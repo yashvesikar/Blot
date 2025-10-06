52dbeb2cf278c58cb1114f720bede44ecd301c40
David Merfield <dmerfield@gmail.com>
2025-10-06 17:59:03 +0200

    Adds script to resync PayPal subscriptions

--- scripts/paypal/get-subscription.js
+++ scripts/paypal/get-subscription.js
@@ -1,33 +1,78 @@
 const config = require("config");
 const get = require("../get/user");
 const fetch = require("node-fetch");
+const getConfirmation = require("../util/getConfirmation");
+const colors = require("colors/safe");
+const User = require("models/user");
+const each = require("../each/user");
 
-async function main (user, callback) {
+async function main(user, callback) {
+  if (!user.paypal || !user.paypal.id) {
+    console.log(
+      colors.dim(
+        "User:",
+        user.uid,
+        user.email,
+        "does not have a PayPal subscription"
+      )
+    );
+    return callback();
+  }
+
   const response = await fetch(
     `${config.paypal.api_base}/v1/billing/subscriptions/${user.paypal.id}`,
     {
       headers: {
         "Content-Type": "application/json",
         "Accept-Language": "en_US",
-        "Authorization": `Basic ${Buffer.from(
+        Authorization: `Basic ${Buffer.from(
           `${config.paypal.client_id}:${config.paypal.secret}`
-        ).toString("base64")}`
-      }
+        ).toString("base64")}`,
+      },
     }
   );
 
   const paypal = await response.json();
 
-  callback(null, paypal);
+  if (!paypal || !paypal.id) {
+    return callback(new Error("No subscription found"));
+  }
+
+  User.set(user.uid, { paypal: paypal }, function (err) {
+    if (err) return callback(err);
+    console.log(
+      colors.green(
+        "User:",
+        user.uid,
+        user.email,
+        "subscription updated from PayPal"
+      )
+    );
+    callback(null);
+  });
 }
 
 module.exports = main;
 
 if (require.main === module) {
-  get(process.argv[2], function (err, user) {
-    main(user, function (err, res) {
-      if (err) throw err;
-      console.log(res);
+  if (process.argv[2]) {
+    get(process.argv[2], function (err, user) {
+      main(user, function (err, res) {
+        if (err) throw err;
+        console.log(res);
+      });
     });
-  });
+  } else {
+    getConfirmation(
+      "Fetch latest subscription information from PayPal for all users? (y/N)",
+      function (err, ok) {
+        if (!ok) return process.exit();
+        each(main, function (err) {
+          if (err) throw err;
+          console.log("Done!");
+          process.exit();
+        });
+      }
+    );
+  }
 }
