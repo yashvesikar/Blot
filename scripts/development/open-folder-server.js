6f9be1591f31634a11f689b2baf21d4aeecce415
David Merfield <dmerfield@gmail.com>
2025-10-13 12:07:29 +0200

    Proxy local client open-folder through dashboard
    * Use dashboard base for local open route
    
    * Point local client open-folder proxy at host helper
    
    * Simplify
    
    * Simplify local open-folder routing
    
    * Simplify open folder helper server

---
+++ scripts/development/open-folder-server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const PORT = Number(process.env.LOCAL_OPEN_FOLDER_PORT) || 3020;
+const BLOGS_DIR = path.resolve(__dirname, "../../data/blogs");
+
+http
+  .createServer((req, res) => {
+    const url = new URL(req.url, "http://localhost");
+
+    if (req.method !== "GET" || url.pathname !== "/open-folder") {
+      res.statusCode = 404;
+      return res.end();
+    }
+
+    const blogID = (url.searchParams.get("blogID") || "").trim();
+
+    if (!blogID) {
+      res.statusCode = 400;
+      return res.end("Missing blogID");
+    }
+
+    spawn("open", [path.join(BLOGS_DIR, blogID)], {
+      detached: true,
+      stdio: "ignore",
+    }).unref();
+
+    res.end("ok");
+  })
+  .listen(PORT, () => {
+    console.log(`Open folder server listening on http://localhost:${PORT}`);
+  });
---
+++ scripts/development/start.js
@@ -0,0 +1,62 @@
+const { spawn } = require("child_process");
+const path = require("path");
+
+const composeArgs = [
+  "-f",
+  path.join(__dirname, "docker-compose.yml"),
+  "up",
+  "--build",
+];
+
+const processes = [];
+
+function spawnProcess(command, args, options = {}) {
+  const child = spawn(command, args, {
+    stdio: "inherit",
+    ...options,
+  });
+
+  processes.push(child);
+
+  child.on("exit", (code, signal) => {
+    if (code && code !== 0) {
+      console.error(`${command} exited with code ${code}`);
+    }
+    if (signal) {
+      console.log(`${command} received signal ${signal}`);
+    }
+  });
+
+  return child;
+}
+
+spawnProcess("node", [path.join(__dirname, "open-folder-server.js")], {
+  env: process.env,
+});
+
+const compose = spawnProcess("docker-compose", composeArgs, {
+  env: process.env,
+});
+
+function shutdown() {
+  for (const child of processes) {
+    if (child.exitCode === null && !child.killed) {
+      child.kill("SIGINT");
+    }
+  }
+}
+
+process.on("SIGINT", () => {
+  shutdown();
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  shutdown();
+  process.exit(0);
+});
+
+compose.on("exit", (code) => {
+  shutdown();
+  process.exit(code || 0);
+});
