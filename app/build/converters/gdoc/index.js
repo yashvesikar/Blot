07c7c334bd59c32fa8b401aabc59cbf17b0c79f8
David Merfield <dmerfield@gmail.com>
2025-10-23 11:36:07 +0200

    Refactor Google Doc converter image handling

--- app/build/converters/gdoc/index.js
+++ app/build/converters/gdoc/index.js
@@ -4,16 +4,15 @@ const LocalPath = require("helper/localPath");
 const extname = require("path").extname;
 const cheerio = require("cheerio");
 const hash = require("helper/hash");
-const fetch = require("node-fetch");
 const { join } = require("path");
 const config = require("config");
-const sharp = require("sharp");
 const Metadata = require("build/metadata");
 const extend = require("helper/extend");
 const yaml = require("yaml");
 
 const blockquotes = require("./blockquotes");
 const linebreaks = require("./linebreaks");
+const processImages = require("./images");
 
 function is(path) {
   return [".gdoc"].indexOf(extname(path).toLowerCase()) > -1;
@@ -122,54 +121,13 @@ async function read(blog, path, callback) {
 
     // remove all inline style attributes
     $("[style]").removeAttr("style");
-
-    console.log("HERE", blog.flags);
     
     // handle line breaks
     if (blog.flags.google_docs_preserve_linebreaks !== false) {
       linebreaks($);
     }
 
-    const images = [];
-
-    $("img").each(function (i, elem) {
-      images.push(elem);
-    });
-
-    for (const elem of images) {
-      const src = $(elem).attr("src");
-
-      try {
-        const res = await fetch(src);
-        const disposition = res.headers.get("content-disposition");
-        const buffer = await res.buffer();
-
-        let ext;
-
-        try {
-          ext = disposition
-            .split(";")
-            .find((i) => i.includes("filename"))
-            .split("=")
-            .pop()
-            .replace(/"/g, "")
-            .split(".")
-            .pop();
-        } catch (err) {}
-
-        if (!ext) {
-          // use sharp to determine the image type
-          const metadata = await sharp(buffer).metadata();
-          ext = metadata.format;
-        }
-
-        const filename = hash(src) + "." + ext;
-        await fs.outputFile(join(assetDir, filename), buffer);
-        $(elem).attr("src", "/_assets/" + hash(path) + "/" + filename);
-      } catch (err) {
-        console.log(err);
-      }
-    }
+    await processImages($, assetDir, path);
 
     // handle blockquotes
     blockquotes($);
