12f15c889ded7642607d3708783d6eaaf8537cd4
David Merfield <dmerfield@gmail.com>
2025-10-30 13:50:54 +0100

    Refactor template font picker overlay

--- app/dashboard/site/template/load/font-inputs.js
+++ app/dashboard/site/template/load/font-inputs.js
@@ -1,10 +1,13 @@
 const font = require("./util/font");
 
 module.exports = function (req, res, next) {
-  res.locals.fonts = Object.keys(req.template.locals)
+  const fonts = Object.keys(req.template.locals)
     .filter(key => key.indexOf("_font") > -1 || key === "font")
     .filter(key => key !== "syntax_highlighter_font")
     .map(key => font(key, req.template.locals[key]));
+
+  res.locals.fonts = fonts;
+  res.locals.fontPickerOptions = font("", {}).options;
 
   return next();
 };
