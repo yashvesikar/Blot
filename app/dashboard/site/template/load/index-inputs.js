32e642fc7f3b032dc37a33a3eee343434b36a31c
David Merfield <dmerfield@gmail.com>
2025-11-07 19:23:58 +0100

    Further improvements to template editor

--- app/dashboard/site/template/load/index-inputs.js
+++ app/dashboard/site/template/load/index-inputs.js
@@ -2,7 +2,7 @@ const determine_input = require("./util/determine-input");
 
 const MAP = {
   page_size: {
-    label: "Posts per page",
+    label: "Number of posts per page",
     min: 1,
     max: 60
   }
