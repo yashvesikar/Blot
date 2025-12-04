0c3369d7f94ef16294135e75a21c5872b87ddc38
David Merfield <dmerfield@gmail.com>
2025-12-04 17:13:57 +0100

    Add error handling to iCloud disconnect route

--- app/clients/icloud/macserver/routes/disconnect.js
+++ app/clients/icloud/macserver/routes/disconnect.js
@@ -2,6 +2,7 @@ const fs = require("fs-extra");
 const { join } = require("path");
 const { iCloudDriveDirectory } = require("../config");
 const { removeLimiterForBlogID } = require("../limiters");
+const { unwatch } = require("../watcher");
 
 module.exports = async (req, res) => {
   const blogID = req.header("blogID");
@@ -20,8 +21,14 @@ module.exports = async (req, res) => {
 
   // remove the blogid folder and the limiter
   removeLimiterForBlogID(blogID);
-  
-  await fs.remove(join(iCloudDriveDirectory, blogID));
+
+  try {
+    await unwatch(blogID);
+    await fs.remove(join(iCloudDriveDirectory, blogID));
+  } catch (error) {
+    console.error(`Failed to disconnect blogID ${blogID}:`, error);
+    return res.status(500).send(error.message);
+  }
 
   console.log(`Disconnected blogID: ${blogID}`);
 
